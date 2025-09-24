456d81777aa322f89a7decf7dba0a1a887aa5b2d
Roms10000 <romain.richardon@outlook.fr>
2025-09-24 09:41:38 +0200

    fetch produit et recette

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
 
-export default function Layout({ boissons }) {
-  return <Outlet context={{ boissons }} />;
+export default function Layout({ boissons, produit, recette }) {
+  return <Outlet context={{ boissons, produit, recette }} />;
 }
