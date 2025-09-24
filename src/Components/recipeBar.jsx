c170b8985e705befb72d12c2d14d76b2a36d34f4
ValentinRibot <valentinribot91@gmail.com>
2025-09-24 09:06:36 +0200

    Recipe page reworked with simplier design

--- src/Components/recipeBar.jsx
+++ src/Components/recipeBar.jsx
@@ -1,15 +1,49 @@
-export function RecipeBar ({ className, Name, Time, Note, Price }) {
+export function RecipeBar ({ className, TextColor, Name, Time, Note, Price, HoverColor }) {
     return(
         <>
             <div className="col-span-8 flex flex-column items-center">
                
-                <div className="w-19/20 h-full flex justify-around items-center recipeBarBackground rounded relative overflow-hidden">
+                <div className=
+                    {` w-19/20 h-full 
+                    flex justify-around items-center 
+                    relative overflow-hidden 
+                    rounded 
+                    recipeBarBackground ${TextColor}`}
+                    >
 
-                    <p className="w-1/5 h-full flex items-center m-0 roboto-bold uppercase">{Name}</p>
-                    <p className="w-1/5 h-full hidden md:flex items-center m-0">Temps:{Time}</p>
-                    <p className="w-1/5 h-full hidden lg:flex items-center m-0">Prix:{Price}</p>
-                    <p className="w-1/5 h-full hidden xl:flex items-center m-0">Note:{Note}</p>
-                    <button className={`${className} w-[120px] h-3/5 rounded roboto-regular`}>See more</button>
+                    <p className="
+                        w-1/5 h-full m-0
+                        flex items-center 
+                        roboto-bold uppercase"
+                        >
+                        {Name}
+                    </p>
+
+                    <p className="
+                        w-1/5 h-full m-0
+                        hidden 
+                        md:flex items-center "
+                        >
+                        Temps:{Time}
+                    </p>
+
+                    <p className="
+                        w-1/5 h-full m-0
+                        hidden 
+                        lg:flex items-center "
+                        >
+                        Prix:{Price}
+                    </p>
+
+                    <p className="
+                        w-1/5 h-full m-0
+                        hidden 
+                        xl:flex items-center "
+                        >
+                        Note:{Note}
+                    </p>
+
+                    <button className={`${className} w-[120px] h-3/5 rounded roboto-regular`} style={{"--drinkColor": HoverColor }}>See more</button>
 
                 </div>
                 
--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -1,76 +1,61 @@
 //Components
 import { Navigation } from "../Components/NavBar"
-import { RecipeBar } from "../Components/recipeBar"
+import { RecipeBar } from "../Components/RecipeBar"
+import { BannerRecipe } from "../Components/BannerRecipe"
 
 export function Recipe() {
 
     const recipeInfo = [
-        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5'},
-        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5'},
-        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5'},
-        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5'},
-        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5'},
-        {nom:"Envou'Temps", temps:"5 min", prix:'20€', note:'4/5'},
-        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5'}
+        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5', color: '#d17d2e'},
+        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5', color: '#5da3c4'},
+        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5', color: '#c50000'},
+        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5', color: '#a88256'},
+        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5', color: '#badd3d'},
+        {nom:"Envou'Temps", temps:"5 min", prix:'20€', note:'4/5', color: '#f172b2'},
+        {nom:"Envou'Temps", temps:"5 min", prix:'10€', note:'4/5', color: '#101010'}
     ]
 
     return (
         <>  
-            <div className="grid grid-rows-[40px_auto_1fr_40px] w-full min-h-screen">
-                
-                {/* Navbar */}
+            <div className="flex flex-col min-h-screen">
+
+                {/* row 1 = navbar */}
                 <div className="bannerBackground">
-                    <Navigation color={'#fff'} className={'invert'}/>
+                    <Navigation color={'#fff'} className={'invert'} />
                 </div>
 
-                {/* Bannière */}
-                <div className="bannerBackground overflow-hidden flex justify-center select-none">
+                {/* row 2 = contenu (bannière + recettes) */}
+                <div className="flex flex-col">
 
-                    <img 
-                        src="/Banner/bannerRecipe_sm.png" 
-                        alt="banner" 
-                        className="h-[100px] min-w-[768px] md:hidden"
-                        draggable="false"
-                    />
+                    {/* Bannière */}
+                    <div className="bannerBackground overflow-hidden flex justify-center select-none">
+                        <BannerRecipe />
+                    </div>
 
-                    <img 
-                        src="/Banner/bannerRecipe_md.png" 
-                        alt="banner" 
-                        className="hidden md:block h-[120px] min-w-[1024px] lg:hidden"
-                        draggable="false"
-                    />
-
-                    <img 
-                        src="/Banner/bannerRecipe_xl.png" 
-                        alt="banner" 
-                        className="hidden lg:block h-[200px] min-w-[1920px]"
-                        draggable="false"
-                    />
-
-                </div>
-
-                {/* Recettes */}
-                <div className="grid grid-cols-8 gap-3 bannerBackground pt-3 pb-3">
-
-                    {recipeInfo.map((element, index) => {
-                        return(
+                    {/* Recettes */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 bannerBackground p-3 auto-rows-[70px]">
+                       
+                        {recipeInfo.map((element, index) => (
                             <RecipeBar 
                                 key={index}
-                                className="recipeButton" 
+                                className="recipeButton"
+                                TextColor="text-white" 
                                 Name={element.nom} 
                                 Time={element.temps} 
                                 Price={element.prix} 
                                 Note={element.note}
+                                HoverColor={element.color}
                             />
-                        )
-                    })}
+                        ))}
+
+                    </div>
 
                 </div>
 
-                {/* Footer */}
+                {/* row 3 = footer */}
                 <div className="bannerBackground"></div>
 
             </div>
-        </>
+        </>                    
     )
 }
