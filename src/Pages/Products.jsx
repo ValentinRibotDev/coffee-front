e0d8d9badd2b03986b962b6501c755c94a2e8345
Roms10000 <romain.richardon@outlook.fr>
2025-09-25 13:45:25 +0200

    recette dans modale recette + petite modif css sur products

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -10,7 +10,7 @@ const { produit } = useOutletContext();
   return (
 <>
   <Navigation/>
-  <div className="flex gap-4 justify-center mt-20">
+  <div className="flex flex-wrap justify-between gap-6 px-6 pt-15 pb-15">
     {produit.map((product) => (
       <div key={product.id} className="w-sm h-auto bg-white border border-gray-200 rounded-lg shadow-sm">
         <div className="flex justify-center">
@@ -22,12 +22,14 @@ const { produit } = useOutletContext();
             {product.description ? product.description.replace(/<[^>]+>/g, '') .replace(/&nbsp;/g, ' ') : ''} {/* suppression de toutes les balises HTML et de l'espace forcé*/} 
           </p>
           <div className="flex justify-between">
-          <p>Intensité : {product.intensity ?? 0}</p> {/* nullish coalescing = si null ou undefined, affiche 0 */}
+          {product.intensity != null && ( 
+            <p>Intensité : {product.intensity}</p>
+          )}
          <p>Origine :&nbsp;<Flag countryCode={product.origin} /></p>
           </div>
           <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-gray-900">{product.price}€</span>
-            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</a>
+            <a href="#" className="text-white bg-orange-200 hover:bg-orange-300 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</a>
         </div>
       </div>
     </div>
