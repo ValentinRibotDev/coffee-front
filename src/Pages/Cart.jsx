a1da609025e274c82c141183d5aa2a54d460a81b
ValentinRibot <valentinribot91@gmail.com>
2025-11-28 10:13:50 +0100

    cart visual done

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -78,36 +78,42 @@ export function Cart() {
         <>
             <Navigation/>
             <h1>Your Cart</h1>
-            <ul>
+            <ul className="m-0 p-2 bg-stone-800">
                 {cart.map((item) => (
-                    <div className="bg-stone-900 text-white p-2 mb-1 border border-white" key={item.id}>
+                    <div className="cartGradient p-2 mb-1 border border-white rounded" key={item.id}>
 
                         <div className="flex justify-between roboto-bold">
-                            <div className="w-1/5">{item.name}</div>                            
-                            <div className="w-1/5">Prix unitaire: ${item.price}</div>
-                            <div className="w-1/5">
-                                Quantité:
-                                <select
-                                    className="bg-black border px-2 ml-1"
-                                    value={item.quantity}
-                                    onChange={(e) =>
-                                        updateQuantity(item.id, Number(e.target.value))
-                                    }
-                                >
-                                    <option value={item.quantity}>
-                                        {item.quantity}
-                                    </option>
-
-                                    {[...Array(10)].map((_, i) => (
-                                        <option key={i + 1} value={i + 1}>
-                                            {i + 1}
+                            <div className="w-1/5 flex justify-start items-center">{item.name}</div>  
+                            <div className="w-3/5 flex flex-col  md:flex-row justify-around items-start md:items-center">
+                                <div className="w-full flex justify-start items-center">Prix unitaire: ${item.price}</div>
+                                <div className="w-full flex justify-start items-center">
+                                    Quantité: 
+                                    <select
+                                        value={item.quantity}
+                                        onChange={(e) => updateQuantity(item.id, e.target.value)}
+                                        className="text-black border rounded"
+                                    >
+                                        {/* Valeur actuelle affichée mais introuvable dans les options */}
+                                        <option value={item.quantity} hidden>
+                                            {item.quantity}
                                         </option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className="w-1/5">Sous-total: ${item.price * item.quantity}</div>
+
+                                        {/* Liste des quantités possibles (1 à 10) sans l'actuelle */}
+                                        {[...Array(10)].map((_, i) => {
+                                            const qty = i + 1;
+                                            return qty !== item.quantity ? (
+                                                <option key={qty} value={qty}>
+                                                    {qty}
+                                                </option>
+                                            ) : null;
+                                        })}
+                                    </select>
+                                </div>
+                                <div className="w-full flex justify-start items-center">Sous-total: ${item.price * item.quantity}</div>    
+                            </div>                          
+                            
                             <div className="w-1/5 flex justify-end">
-                                <button className="bg-red-500 text-white p-1 w-[30px] rounded" onClick={() => removeFromCart(item.id)}>X</button>
+                                <button className="bg-red-500 text-white p-1 w-[60px] rounded" onClick={() => removeFromCart(item.id)}>X</button>
                             </div>
                         </div>
                          
